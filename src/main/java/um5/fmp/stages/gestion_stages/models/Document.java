bb65030cf3a65b3c973d1bc64f9bdca8c1d65b49
Imadbenali19 <benali.ib19@gmail.com>
2022-07-06 18:53:57 +0000

    Commit changes

--- src/main/java/um5/fmp/stages/gestion_stages/models/Document.java
+++ src/main/java/um5/fmp/stages/gestion_stages/models/Document.java
@@ -19,6 +19,8 @@ public class Document {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String lien;
+    private String description;
+    private String type;
 
     @ManyToOne
     @JsonIgnoreProperties("{documents}")
